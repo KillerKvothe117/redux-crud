24757618432a71092b93f998f4db30fea42bc4f1
Kosi <iyiegbuk@gmail.com>
2022-07-07 08:53:44 +0100

    implemented delete user function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,61 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addUser, deleteUser } from "./features/users";
+import uuid from "react-uuid";
 
 function App() {
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  
+
+  const userList = useSelector((state) => state.users.value);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
+      <div className="addUser">
+        <input
+          type={"text"}
+          placeholder="Name..."
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <input
+          type={"text"}
+          placeholder="Username..."
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            dispatch(addUser({ id: uuid(), name, username }));
+          }}
+        >
+          Add User
+        </button>
+      </div>
+      <div className="displayUsers">
+        {userList.map((user) => {
+          return (
+            <div>
+              <h1>{user.name}</h1>
+              <h1>{user.username}</h1>
+              <input type="text" placeholder="New username..." />
+              <button>Update Username</button>
+              <button
+                onClick={() => {
+                  dispatch(deleteUser({ id: user.id }));
+                }}
+              >
+                Delete User
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/features/users.jsx
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { UsersData } from "../data";
+
+export const userSlice = createSlice({
+  name: "users",
+  initialState: { value: UsersData },
+  reducers: {
+    addUser: (state, action) => {
+      state.value.push(action.payload);
+    },
+
+    deleteUser: (state, action) => {
+      state.value = state.value.filter((user) => user.id !== action.payload.id);
+    },
+  },
+});
+
+export const { addUser, deleteUser } = userSlice.actions;
+export default userSlice.reducer;
